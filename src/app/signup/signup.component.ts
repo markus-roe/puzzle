3bb6b0bca0e5d4bf84e5b2bd49957970205a93ba
Markus Rösner <markus.roesner@gmx.net>
2023-05-09 09:20:20 +0200

    added routing

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,35 +1,3 @@
-// import { Component } from '@angular/core';
-// import { FormControl, Validators } from '@angular/forms';
-
-// @Component({
-//   selector: 'signup',
-//   templateUrl: './signup.component.html',
-//   styleUrls: ['./signup.component.css']
-// })
-// export class SignUpComponent {
-//   hidePassword = true;
-//   hideConfirmPassword = true;
-
-//   emailControl = new FormControl('', [Validators.required, Validators.email]);
-//   passwordControl = new FormControl('', Validators.required);
-//   confirmPasswordControl = new FormControl('', Validators.required);
-//   addressControl = new FormControl('', Validators.required);
-//   cityControl = new FormControl('', Validators.required);
-//   zipControl = new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")]);
-
-//   submit() {
-//     if (this.emailControl.invalid || this.passwordControl.invalid || this.confirmPasswordControl.invalid || this.addressControl.invalid || this.cityControl.invalid || this.zipControl.invalid) {
-//       console.error("Form is invalid!");
-//       return;
-//     }
-
-//     if (this.passwordControl.value !== this.confirmPasswordControl.value) {
-//       console.error("Passwords do not match!");
-//       return;
-//     }
-
-//   }
-// }
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -39,6 +7,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignUpComponent {
 
   constructor(private snackBar: MatSnackBar) { }
